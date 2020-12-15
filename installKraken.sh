3f6d5194252aaac08538f03b866fbd2a4237cf57
Ky <kay.suttkus@gmail.com>
2020-12-15 21:41:07 +0100

    add  checkDocker

--- installKraken.sh
+++ installKraken.sh
@@ -6,19 +6,28 @@ echo "Install Kraken.Charge"
 echo "Get update"
 sudo apt-get update
 
-echo "Install Docker"
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-sudo usermod -aG docker Pi
-docker version
-docker info
-
+dockerState = docker -v 
+echo dockerState
+if [isDocker == 0];
+then
+    echo "Install Docker"
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker pi
+    docker version
+    docker info
+else
+    echo "Docker is already installed."
+    echo "Docker installation was skipped"
+fi
 echo "Configuration of serial port"
 raspi-config nonint do_serial 1
 
 echo "Deactivate Bluetooth"
 echo "dtoverlay=disable-bt" | sudo tee -a /boot/config.txt
 sudo systemctl disable hciuart
+
+docker run -it -p 8000:80 --mount source=krakenData,target=/app/database --mount source=krakenLogs,target=/app/wwwroot/logs  --device=/dev/serial0  --restart=always kyle236/krakenhome:latest
 
 echo "The system will reboot in 10 seconds to finish the installation."
 sleep 10
